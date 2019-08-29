587dd863242a4afaf015486766a0e68b21057b73
ktscates <cates2506@gmail.com>
2019-08-29 17:40:24 +0200

    Update search component file

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchRequestService } from '../search-http/search-request.service';
+import { User } from '../user';
 
 
 @Component({
@@ -7,11 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+
+  user: User;
   
 
-  constructor() { }
+  constructor(private searchService:SearchRequestService) { }
 
   ngOnInit() {
+    this.searchService.userRequest("")
+    this.user = this.searchService.user
 
   }
 
